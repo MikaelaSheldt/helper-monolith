d7186ef95b9dc51f26705894890c68489828a035
Mikaela Sheldt <msheldt@gmail.com>
2019-05-23 15:39:46 -0400

    EDIT(api/blocks) make more restful

--- api/blocks.js
+++ api/blocks.js
@@ -24,19 +24,6 @@ router.post('/', async (req, res, next) => {
   }
 })
 
-router.get('/run/:minutes', async (req, res, next) => {
-  const minutes = req.params.minutes
-  console.log('MINUTES', req.params.minutes);
-  try{
-    const matches = await Block.findAll({
-      where: {expectedDuration: minutes}
-    })
-    res.json(matches)
-  } catch (err) {
-    next(err)
-  }
-})
-
 // matches GET requests to /api/blocks/:blockId
 router.get('/:blockId', async (req, res, next) => {
   try {
@@ -52,38 +39,52 @@ router.put('/:blockId', async (req, res, next) => {
   try {
     const block = await Block.findById(req.params.blockId)
     const updatedBlock = block.update(req.body)
-    console.log('updatedBlock ', block);
+    console.log('updatedBlock ', block.description);
     res.json(block)
   } catch (err) {
     next(err)
   }
 })
 
-router.put('/reset/status', async (req, res, next) => {
-  try {
-    const blocks = await Block.scope(null).findAll(
-      {where: {status: req.body.status}}
-    )
-    blocks.forEach(async (block) => {
-      await block.update({status: 'active'})
-      console.log(`${block.id} has been updated to ${block.status}`);
+// matches GET requests to /api/blocks/
+router.get('/time/:minutes', async (req, res, next) => {
+  const minutes = req.params.minutes
+  try{
+    const matches = await Block.findAll({
+      where: {duration: minutes}
     })
-    res.send('done')
+    res.json(matches)
   } catch (err) {
     next(err)
   }
 })
 
-router.put('/pause/:blockId', async (req, res, next) => {
-  try {
-    const block = await Block.findById(req.params.blockId)
-    const updatedBlock = block.update(req.body)
-    console.log('updatedBlock ', block);
-    res.json(block)
-  } catch (err) {
-    next(err)
-  }
-})
+// router.put('/status/:status', async (req, res, next) => {
+//   try {
+//     const blocks = await Block.scope(null).findAll(
+//       {where: {status: req.params.status}}
+//     )
+//     blocks.forEach(async (block) => {
+//       await block.update({status: 'active'})
+//       console.log(`${block.description} has been updated to ${block.status}`);
+//     })
+//     res.send('done')
+//   } catch (err) {
+//     next(err)
+//   }
+// })
+//
+// router.put('/pause/:blockId', async (req, res, next) => {
+//   try {
+//     const block = await Block.findById(req.params.blockId)
+//     const updatedBlock = block.update(req.body)
+//     console.log('updatedBlock ', block.description);
+//     res.json(block)
+//   } catch (err) {
+//     next(err)
+//   }
+// })
+
 // matches DELETE requests to /api/blocks/:blockId
 router.delete('/:blockId', async (req, res, next) => {
   try {
